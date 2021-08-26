5ef297ef558664324873d9d5cc504a309dd33afd
Kianek <kallridge92@gmail.com>
2021-08-25 20:02:06 -0700

    Initial commit

---
+++ HospitalManager.Api/Patients/PatientIntake.cs
@@ -0,0 +1,8 @@
+namespace HospitalManager.Api.Patients
+{
+    public class PatientIntake
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
