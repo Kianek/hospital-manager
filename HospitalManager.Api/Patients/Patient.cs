e7047c821f8e3cab20373e355196912ab98072ce
Kianek <kallridge92@gmail.com>
2021-08-26 07:32:03 -0700

    Add setter for Bed property

--- HospitalManager.Api/Patients/Patient.cs
+++ HospitalManager.Api/Patients/Patient.cs
@@ -7,13 +7,24 @@ namespace HospitalManager.Api.Patients
 {
     public class Patient : Entity
     {
+        private Guid? _bedId;
+        private Bed? _bed;
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
         public Guid RoomId { get; set; }
         public Room Room { get; set; }
         public Guid? BedId { get; set; }
-        public Bed? Bed { get; set; }
+
+        public Bed? Bed
+        {
+            get => _bed;
+            set
+            {
+                _bed = value;
+                _bedId = Bed?.Id;
+            }
+        }
         
         public Patient(string firstName, string lastName)
         {
