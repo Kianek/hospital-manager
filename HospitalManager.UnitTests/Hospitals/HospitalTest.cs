19a62ce0f578551df877e73385472a0ce5d7c37f
Kianek <kallridge92@gmail.com>
2021-08-25 17:22:47 -0700

    Initial commit

---
+++ HospitalManager.UnitTests/Hospitals/HospitalTest.cs
@@ -0,0 +1,38 @@
+using System;
+using HospitalManager.Api.Hospitals;
+using Xunit;
+
+namespace HospitalManager.UnitTests.Hospitals
+{
+    public class HospitalTest
+    {
+        [Fact]
+        public void Hospital_NameAndNumberOfBedsValid_InstantiatesSuccessfully()
+        {
+            var name = "Some Hospital";
+            var hospital = new Hospital(name, 50);
+            
+            Assert.True(hospital != null);
+            Assert.True(hospital.Name == name);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void Hospital_NumberOfRoomsZeroOrLess_ThrowsArgumentOutOfRangeException(int rooms)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => Helpers.GetHospital(numberOfRooms: rooms));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("Snazzy#($&Hospital")]
+        [InlineData("Still.Invalid")]
+        public void Hospital_NameEmptyOrIncludesInvalidCharacters_ThrowsArgumentException(string name)
+        {
+            Assert.Throws<ArgumentException>(() => Helpers.GetHospital(name));
+        }
+    }
+}
