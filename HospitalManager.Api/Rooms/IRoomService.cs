522fd4042aceb97c6a83abd8e72c237489cdcc4d
Kianek <kallridge92@gmail.com>
2021-08-26 20:31:40 -0700

    Remove unused import

--- HospitalManager.Api/Rooms/IRoomService.cs
+++ HospitalManager.Api/Rooms/IRoomService.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using HospitalManager.Api.Beds;
 using HospitalManager.Api.Patients;
 
 namespace HospitalManager.Api.Rooms
