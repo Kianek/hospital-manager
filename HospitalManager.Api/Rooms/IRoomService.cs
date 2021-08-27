257d675f3ae78059982d94dbf72f0da2b6f2a6db
Kianek <kallridge92@gmail.com>
2021-08-26 17:10:52 -0700

    Change some params & return types

--- HospitalManager.Api/Rooms/IRoomService.cs
+++ HospitalManager.Api/Rooms/IRoomService.cs
@@ -1,17 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using HospitalManager.Api.Beds;
 using HospitalManager.Api.Patients;
 
 namespace HospitalManager.Api.Rooms
 {
     public interface IRoomService
     {
-        Task<Room> GetRoomById(Guid roomId);
-        Task<Room> GetRoomByRoomNumber(int roomNumber);
-        Task<IEnumerable<Room>> GetRoomsByHospitalName(string name);
-        Task AddBedsToRoom(int numberOfBedsToAdd = 1);
-        Task RemoveBedFromRoom(Guid bedId);
-        Task AssignPatient(PatientBedAssignment assignment);
+        Task<Room> GetRoomById(string hospitalName, Guid roomId);
+        Task<Room> GetRoomByRoomNumber(string hospitalName, int roomNumber);
+        Task<List<Room>> GetRoomsByHospitalName(string name);
+        Task<Bed> AddBedsToRoom(RoomBedOrder order);
+        Task<bool> RemoveBedFromRoom(Guid bedId);
+        Task<bool> AssignPatient(PatientBedAssignment assignment);
     }
 }
