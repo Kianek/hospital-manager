1871a8490802b978ea19ceace283c30c981bbebd
Kianek <kallridge92@gmail.com>
2021-08-27 10:09:36 -0700

    Implement methods

--- HospitalManager.Api/Hospitals/HospitalsController.cs
+++ HospitalManager.Api/Hospitals/HospitalsController.cs
@@ -1,5 +1,6 @@
-using System;
+using System.Linq;
 using System.Threading.Tasks;
+using HospitalManager.Api.Rooms;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HospitalManager.Api.Hospitals
@@ -9,34 +10,57 @@ namespace HospitalManager.Api.Hospitals
     public class HospitalsController : ControllerBase
     {
         private readonly IHospitalService _hospitalService;
+        private readonly IRoomService _roomService;
 
-        public HospitalsController(IHospitalService hospitalService)
+        public HospitalsController(IHospitalService hospitalService, IRoomService roomService)
         {
             _hospitalService = hospitalService;
+            _roomService = roomService;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAllHospitals()
         {
-            throw new NotImplementedException();
-        }
-
-        [HttpGet("{hospitalId:guid}")]
-        public async Task<IActionResult> GetHospitalById(Guid hospitalId)
-        {
-            throw new NotImplementedException();
+            var result = await _hospitalService.GetAllHospitals();
+            return Ok(result);
         }
 
         [HttpGet("{name:alpha}")]
         public async Task<IActionResult> GetHospitalByName(string name)
         {
-            throw new NotImplementedException();
+            var result = await _hospitalService.GetHospitalByName(name);
+
+            if (result is null) return NotFound(name);
+
+            return Ok(result.AsDto());
+        }
+        
+        [HttpGet("{hospitalName:alpha}/rooms")]
+        public async Task<IActionResult> GetAllHospitalRooms(string hospitalName)
+        {
+            var result =  await _roomService.GetRoomsByHospitalName(hospitalName);
+            
+            return Ok(result.Select(r => r.AsDto()).ToList());
+        }
+        
+        [HttpGet("{hospital:alpha}/rooms/{roomNumber:int}")]
+        public async Task<IActionResult> GetHospitalRoomByNumber(string hospital, int roomNumber)
+        {
+            var result = await _roomService.GetRoomByRoomNumber(hospital, roomNumber);
+
+            if (result is null) return NotFound(new { Hospital = hospital, RoomId = roomNumber });
+            
+            return Ok(result.AsDto());
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateHospital(HospitalInfo hospital)
         {
-            throw new NotImplementedException();
+            var result = await _hospitalService.CreateHospital(hospital);
+
+            if (result is null) return BadRequest();
+
+            return CreatedAtAction(nameof(CreateHospital), result.AsDto());
         }
     }
 }
