6e1412f44928e28d66070568ff8051269df75e2c
Kianek <kallridge92@gmail.com>
2021-08-27 09:49:11 -0700

    Initial commit

---
+++ HospitalManager.Api/Migrations/20210827164842_UpdateRoomModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HospitalManager.Api.Migrations
+{
+    public partial class UpdateRoomModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HospitalName",
+                table: "Rooms",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HospitalName",
+                table: "Rooms");
+        }
+    }
+}
