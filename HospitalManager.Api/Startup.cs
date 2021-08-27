df08c16462f198a3eb4cce9afdc1eed93d85d7fd
Kianek <kallridge92@gmail.com>
2021-08-27 10:06:17 -0700

    Add NewtonsoftJson

--- HospitalManager.Api/Startup.cs
+++ HospitalManager.Api/Startup.cs
@@ -32,7 +32,11 @@ namespace HospitalManager.Api
             services.AddDbContext<AppDbContext>(options => 
                 options.UseSqlite(Configuration[ConnectionString]));
             
-            services.AddControllers();
+            services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
